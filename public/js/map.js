780681ecc1089c8b0ca0b4f10cc947bc8bd560ed
Adam Stelle <adamstelle@gmail.com>
2016-07-01 16:55:30 -0700

    Add view and js file for map; added map to css

---
+++ public/js/map.js
@@ -0,0 +1,17 @@
+var projection = d3.geo.mercator();
+
+var svg = d3.select("#map").append("svg")
+  .attr("width", width)
+  .attr("height", height);
+var path = d3.geo.path()
+  .projection(projection);
+var g = svg.append("g");
+
+d3.json("world-110m2.json", function(error, topology) {
+          g.selectAll("path")
+            .data(topojson.object(topology, topology.objects.countries)
+                .geometries)
+          .enter()
+            .append("path")
+            .attr("d", path)
+});
