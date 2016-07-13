c8ceffbbc062c129e3b0a7cc4f5a82fcd08fc29f
Adam Stelle <adamstelle@gmail.com>
2016-07-12 19:56:21 -0700

    Add basic client side logic & functions

--- public/js/main.js
+++ public/js/main.js
@@ -167,28 +167,63 @@ function sortResults(counts, numDays) {
   }
 }
 
-// var mymap = L.map("map", {
-//   center: [47.6062, -122.3321],
-//   zoom: 12,
-//   scrollWheelZoom: false
-// });
-//
-// // Load map
-// L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWRhbXN0ZWxsZSIsImEiOiJjaXE1Y2JyYWkwMDU0ZmxtNnpxYmxnaDlwIn0.4Uq9e1A2t0jjoQQ_oyF_fQ', {
-//     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox://styles/mapbox/light-v9',
-// }).addTo(mymap);
-//
-// // Add 300 most recent incidents to map
-// function addMarkers(myjson) {
-//   var myArr = $.map(myjson, function(el) {return el});
-//   for(i=myArr.length-1; i>myArr.length-300; i--) {
-//       L.marker(L.latLng(myArr[i]["latitude"],myArr[i]["longitude"])).addTo(mymap);
-//   }
-// }
+var mymap = L.map("map", {
+  center: [47.6062, -122.3321],
+  zoom: 12,
+  scrollWheelZoom: false
+});
+
+// Load map
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWRhbXN0ZWxsZSIsImEiOiJjaXE1Y2JyYWkwMDU0ZmxtNnpxYmxnaDlwIn0.4Uq9e1A2t0jjoQQ_oyF_fQ', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox://styles/mapbox/light-v9',
+}).addTo(mymap);
+
+// Add 300 most recent incidents to map
+function addMarkers(myjson) {
+  var myArr = $.map(myjson, function(el) {return el});
+  for(i=myArr.length-1; i>myArr.length-300; i--) {
+      L.marker(L.latLng(myArr[i]["latitude"],myArr[i]["longitude"])).addTo(mymap);
+  }
+}
 
 // Realtime address auto-completion
-$("#address").on(keyStroke), function() {
-  $("#test").hide();
-}
+
+// Address validation / verification
+
+// Retrieve and clean submitted address
+$("#addressSubmit").on("click", function getAddress() {
+  var userAddress = $("#address").val();
+  getResults(userAddress);
+});
+
+// // Convert to latlng
+// function getResults(userAddress) {
+//   var coordinates      = getCoordinates(userAddress);
+//   var userNeigh        = getNeighborhoods(coordinates);
+//   var neighborhoodData = getNeighJSON(userNeigh);
+//   showNeighborhoodResults(userNeigh);
+// }
+//
+// // Get Coordinates from address (nested function)
+// function getCoordinates(userAddress) {
+//   // coordinates = ...
+//   return coordinates;
+// }
+//
+// // Define neighborhood (nested function)
+// function getNeighborhoods(coordinates) {
+//   // userNeigh = ...
+//   return userNeigh;
+// }
+//
+// function getNeighJSON(userNeigh) {
+//   // Retrieve JSON data from server specific to neighborhood
+//   $.getJSON(...)
+// }
+//
+// function showNeighborhoodResults(userNeigh) {
+//   // Display key data from JSON (last 20 crimes; prevalence of overall crime & specific types vs city avg);
+//   // Display Leaflet map w/last X# crimes, centered on latlng
+// }
