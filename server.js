9d3c45060d64e23ec8804f1ca42d35f0d80c9953
Adam Stelle <adamstelle@gmail.com>
2016-07-26 14:39:53 -0700

    Turn on core function

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ function mapIncidents(myjson) {
     thisHood["diffFromAvg"]  = Math.round((thisHood["numIncidents"] / (resultsByHood.length / numHoods))*10)/10;
     myNeighborhood.push(thisHood);
   });
-  
+
   // Convert to JSON
   var jsonResults = JSON.stringify(myNeighborhood);
   return jsonResults;
@@ -154,7 +154,7 @@ function saveData(json) {
 }
 
 // Initiate data gathering & storage - THIS SHOULD BE RECURRING DAILY
-// getIncidentData(saveData);
+getIncidentData(saveData);
 
 // Retrieve neighborhood data from JSON file based on user address
 function retrieveHoodData(userHood, userSubHood) {
