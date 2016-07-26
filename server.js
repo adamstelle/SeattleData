bb6a077b4e9c8fbed49cfa95f0bff8b8357f6c64
Adam Stelle <adamstelle@gmail.com>
2016-07-26 16:09:46 -0700

    Modified leaflet module source

--- server.js
+++ server.js
@@ -153,7 +153,7 @@ function saveData(json) {
 }
 
 // Initiate data gathering & storage - THIS SHOULD BE RECURRING DAILY
-getIncidentData(saveData);
+// getIncidentData(saveData);
 
 // Retrieve neighborhood data from JSON file based on user address
 function retrieveHoodData(userHood, userSubHood) {
